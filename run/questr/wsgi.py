37650ef57c389a2133c835cc269398696fcadb03
Chuhan Qin <cqin5@uwo.ca>
2014-03-22 15:19:20 -0400

    Removed Heroku Test

--- run/questr/wsgi.py
+++ run/questr/wsgi.py
@@ -12,7 +12,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "questr.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-
-from helloworld.wsgi import HelloWorldApplication
-application = HelloWorldApplication(application)
