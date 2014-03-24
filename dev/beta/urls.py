74ee3e43222bb3234e94d54ea6f16ef76942b01d
Chuhan Qin <cqin5@uwo.ca>
2014-03-24 12:11:26 -0400

    Deloyed Beta on Heroku
    Heroku, venv, etc

--- dev/beta/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from beta import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^join_beta/', views.join_beta, name='join_beta'),
-    url(r'^thankyou/', views.thankyou, name='thankyou'),
-)
--- run/beta/models.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from django.db import models
-
-class Contact(models.Model):
-	email = models.EmailField()
