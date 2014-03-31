c907a503632af7366c54f92b0765969eb24e1e9c
Gaurav Ghimire <gaurav@subisu.net.np>
2014-03-31 20:42:56 +0545

    postgres db connection established

--- beta/views.py
+++ beta/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 from django.template import RequestContext, loader
+from models import Contact
+import mailchimp
 
 def index(request):
     return render(request, 'beta/index.html')
@@ -12,11 +14,11 @@ def thankyou(request):
 
 def join(request):
 	m = mailchimp.Mailchimp('0bcdb957ee18611ed49045a5a1df196d-us8')
-	new_user = Contact( email = request.POST['email'])
+	new_user = Contact( email = request.POST['EMAIL'])
 	new_user.save()
-	try:
-		m.helper.ping()
-	except mailchimp.Error:
-		messages.error(request,  "Invalid API key")
+	# try:
+	# 	m.helper.ping()
+	# except mailchimp.Error:
+	# 	messages.error(request,  "Invalid API key")
 	#return HttpResponseRedirect(reverse('beta:thankyou'))
-	return render(request, 'beta/index.html')
+	return render(request, 'beta/thankyou.html', locals())
