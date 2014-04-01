02f13f8391848a4ba9d6a1a85a4d1987649d18d5
Ajan Lal Shrestha <ajan@nixify.com>
2014-04-01 22:48:45 +0545

    Shifted the validations and pylib into a libs package and also checked the duplicate email from mailchimp API

--- beta/views.py
+++ beta/views.py
@@ -3,13 +3,12 @@ from django.shortcuts import render
 # from django.core.urlresolvers import reverse
 from django.contrib import messages
 # from django.template import RequestContext, loader
+
+
+import mailchimp
+import logging as log
 from models import Contact
-from questr.globals import validateEmail
-
-import os, mailchimp
-
-API_KEY = os.environ['MAILCHIMP_API_KEY']
-LIST_ID = os.environ['MAILCHIMP_BETA_INVITATION_LIST_ID']
+from libs import mailchimp_handler, validations
 
 def index(request):
     return render(request, 'beta/index.html')
@@ -19,21 +18,34 @@ def index(request):
 # 	return render(request, 'beta/thankyou.html', locals())
 
 # function to join the invitee's subscription list
+
 def join(request):
-	mcObject = mailchimp.Mailchimp(API_KEY)
 	email = request.POST['EMAIL']
-	isCorrect = validateEmail(email)
-	if not isCorrect:
-		messageResponse="Please provide with a valid email address!"
-		return render(request, 'beta/thankyou.html', locals())
-	new_user = Contact( email = email)
-	new_user.save()
-	try:
-		mcObject.lists.subscribe(LIST_ID, {"email":email})
-		mcObject.helper.ping()
-	except mailchimp.Error:
-		messages.error(request,  "Invalid API key")
-	messageResponse = "Thanks for joining us.<br>Please check your mailbox.</b>"
-	# return HttpResponseRedirect(reverse('beta:thankyou'))	
-	return render(request, 'beta/thankyou.html', locals())
+	if email:
+		if validations.is_valid_email(email):
+			try:
+				response = mailchimp_handler.subscribe(email)
+				if not response:
+					messageResponse = "You have already been subscribed"
+					return render(request, 'beta/index.html', locals())
+				else:
+					mailchimp_handler.ping()
+					
+					new_user = Contact( email = email)
+					new_user.save()
+					
+					messageResponse = "Thanks for joining us.<br>Please check your mailbox.</b>"
+					# return HttpResponseRedirect(reverse('beta:thankyou'))	
+					return render(request, 'beta/thankyou.html', locals())
+			except mailchimp.Error, e:
+				# Error for 
+				log.debug(str(e))
+				messageResponse = "Something went wrong"
+				return render(request, 'beta/thankyou.html', locals())
+		else:
+			messageResponse="Please provide with a valid email address!"
+			return render(request, 'beta/thankyou.html', locals())
+	else:
+		messageResponse = "Please enter the email address"
+		return render(request, 'beta/index.html', locals())
 
