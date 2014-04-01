91e2614f29aaa6d2f32ee94167478ce14cb08867
Gaurav Ghimire <exsys@nixify.com>
2014-04-02 00:19:13 +0545

    rectified merger issues

--- questr/settings.py
+++ questr/settings.py
@@ -41,7 +41,6 @@ DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
->>>>>>> dev
 
 # Application definition
 import mailchimp
