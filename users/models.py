7a50b6cbf7d9c7e61a30b0a7f96215ac3f0cfd48
Ajan Lal Shrestha <ajan@nixify.com>
2014-05-06 23:12:20 +0545

    Added the models, views and templates for email verification. There is token time for every transactional mail after which it runs out of validity. Hashed the user id to block bots in transactional email for identification Added decorators for verification and time constraint Two models for transactional mail mapping and token is created for handling the whole process.

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 
 
+import hashlib
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
-import logging
+
 
 # Create your models here.
 class QuestrUserManager(BaseUserManager):
@@ -55,3 +56,52 @@ class QuestrUserProfile(AbstractBaseUser):
 
     def __unicode__(self):
         return self.email 
+
+# User transactionl model
+class UserTransactional(models.Model):
+    id = models.IntegerField(_('id'), primary_key=True)
+    user_code = models.CharField(_('user_code'), max_length=64, null=False, unique=True)
+    email = models.EmailField(_('email'), max_length=100, blank=False, null=False, unique=True)
+    status = models.BooleanField(_('status'), default=False, blank=False)
+
+    def generate_hash(self):
+        return hashlib.sha256(str(self.email)).hexdigest()
+
+    def get_truncated_user_code(self):
+        return self.user_code[:7]
+
+    def get_token_id(self):
+        return self.user_code[-6:]
+
+    REQUIRED_FIELDS = ['user_code', 'id']
+
+    def __unicode__(self):
+        return "{0}:{1} {2}".format(self.user_code, self.id, self.email)
+
+    #Overriding
+    def save(self, *args, **kwargs):
+        #check if the row with this hash already exists.
+        if not self.user_code:
+            self.user_code = self.generate_hash()
+        # self.my_stuff = 'something I want to save in that field'
+        super(UserTransactional, self).save(*args, **kwargs)
+
+# Questr Token
+class QuestrToken(models.Model):
+    token_id = models.CharField(_('id'), max_length=20, primary_key=True)
+    timeframe = models.DateTimeField(_('create_date'), default=timezone.now)
+
+    def is_alive(self):
+        timedelta = timezone.now() - self.timeframe
+        days = 4
+        allowable_time = float(days * 24 * 60 * 60)
+        return timedelta.total_seconds() < allowable_time
+
+    def __unicode__(self):
+        return "Token verifying ..."
+
+    # Overriding
+    def save(self, *args, **kwargs):
+        if not self.timeframe:
+            self.timeframe = timezone.now()
+        super(QuestrToken, self).save(*args, **kwargs)
