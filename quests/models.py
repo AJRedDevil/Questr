0e753c37b92c5a32173967f0e6c4a38da333aee9
Gaurav Ghimire <exsys@nixify.com>
2014-05-23 08:52:17 +0545

    edited quest models for blanks, added href for editing quests

--- quests/models.py
+++ quests/models.py
@@ -9,22 +9,22 @@ class Quests(models.Model):
     questrs = models.ForeignKey(QuestrUserProfile)
     pretty_url = models.CharField(_('pretty_url'), 
         max_length=100, blank=False)
-    description = models.TextField(_('description'), blank=False)
+    description = models.TextField(_('description'))
     title = models.CharField(_('title'), max_length=100, 
         blank=False)
     reward = models.DecimalField(_('reward'), decimal_places=2, 
         max_digits=1000, blank=False)
     item_images = models.URLField(_('item_images'), blank=True)
-    status = models.IntegerField(_('status'), blank=False)
+    status = models.IntegerField(_('status'))
     creation_date = models.DateField(_('creation_date'), 
         blank=False)
-    rating = models.IntegerField(_('rating'), blank=False)
-    offerers = models.TextField(_('questrs'), blank=False) # if posted under an offer this would be a single digit (pk of questr object of the offerer)
+    rating = models.IntegerField(_('rating'))
+    offerers = models.TextField(_('questrs')) # if posted under an offer this would be a single digit (pk of questr object of the offerer)
     qr_code = models.URLField(_('qr_code'), blank=True)
-    srcaddress = models.TextField(_('srcaddress'), blank=False) # this would be a dict of address attributes 
-    dstaddress = models.TextField(_('dstaddress'), blank=False) # this would be a dict of address attributes
-    isprivate = models.BooleanField(_('isprivate'), default=True, blank=False) # if posted under an offer this would be always set to True, else would be set as False
-    isaccepted = models.BooleanField(_('isaccepted'), default=False, blank=False)
+    srcaddress = models.TextField(_('srcaddress')) # this would be a dict of address attributes 
+    dstaddress = models.TextField(_('dstaddress')) # this would be a dict of address attributes
+    isprivate = models.BooleanField(_('isprivate'), default=True) # if posted under an offer this would be always set to True, else would be set as False
+    isaccepted = models.BooleanField(_('isaccepted'), default=False)
 
     def __unicode__(self):
         return self.pretty_url 
@@ -33,8 +33,8 @@ class Quests(models.Model):
 class QuestComments(models.Model):
     quest = models.ForeignKey(Quests)
     questr = models.ForeignKey(QuestrUserProfile)
-    time = models.DateTimeField(_('time'), blank=False)
-    comment = models.TextField(_('comment'), blank=False)
+    time = models.DateTimeField(_('time'))
+    comment = models.TextField(_('comment'))
 
     def __unicode__(self):
         return self.id
