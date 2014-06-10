c7b4d5070d004c7aa8ab78be861eccd098ea2d3a
Ajan Lal Shrestha <ajan@nixify.com>
2014-06-11 00:34:08 +0545

    Updated the Reset Password Created template for reset password Added reset password mail sent message in homepage Updated the id for auto increment in user transactional table

--- users/contrib/mailing.py
+++ users/contrib/mailing.py
@@ -32,7 +32,40 @@ def send_verification_email(user, verf_link):
     template_name = "Welcome_Email"
     try:
         msg = __get_verification_template(template_name, user.first_name, user.email, verf_link)
-        logging.warn("Assumption email Sent")
+        logging.debug("Verification email Sent")
+        msg.send()
+    except Exception, e:
+        logging.exception(str(e))
+
+def __get_reset_template(template_name, first_name, email, new_password):
+    """
+    Returns the template for email verification.
+    """
+    ##Email Template Init###
+    email_template = EmailMessage(
+        subject="Password has been reset",
+        from_email="Questr <hello@questr.co>",
+        to=[email],
+        headers={'Reply-To': "Questr <hello@questr.co>"}
+        )
+
+    ###List Email Template###
+    email_template.template_name = template_name
+
+    ###List Email Tags to be used###
+    email_template.global_merge_vars = { 'NEW_PASSWORD'  : new_password, 'COMPANY' : 'Questr Co', 'FIRST_NAME' : first_name , }
+
+    return email_template
+
+def send_reset_password_email(user, new_password):
+    """
+    Send password reset mail to the user.
+    """
+    #Mailchimp Template
+    template_name = "RESET_PASSWORD_EMAIL"
+    try:
+        msg = __get_reset_template(template_name, user.first_name, user.email, new_password)
+        logging.debug("Reset password email Sent")
         msg.send()
     except Exception, e:
         logging.exception(str(e))
