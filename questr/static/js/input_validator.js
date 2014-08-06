b5ef60ea610a3d20763227f2506ccd22243fc549
Chuhan Qin <cqin@Chuhans-MacBook-Pro.local>
2014-08-06 00:22:58 -0400

    Added password and email validations

--- questr/static/js/input_validator.js
+++ questr/static/js/input_validator.js
@@ -31,3 +31,21 @@ function validatePostalAndZipCode(postalcode) {
     }
 }
 
+/* Check if two passwords match */
+function validatePasswords() {
+    var password1 = $("#password1").value;
+    var password2 = $("#password2").value;
+
+    if(password1 != password2) {
+        warning(password2);
+    }
+    
+}
+
+/* Check if input is a valid email address. Meets RFC2822 grammar. */
+function validateEmailAddress(emailAddress) {
+    var pattern = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+    if (!pattern.test(postalcode)){
+        warning(postalcode);
+    }
+}
