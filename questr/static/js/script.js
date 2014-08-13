c02ee0bc670713b682a81f714f3ad4c4b8b62e63
vvo3@uwo.ca <vietcavo@Vietcas-MacBook-Pro.local>
2014-08-12 23:30:17 -0400

    ignore

--- questr/static/js/script.js
+++ questr/static/js/script.js
@@ -1,9 +1,30 @@
+
+    var app = angular.module('myApp', []);
+    app.directive('match', function () {
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            scope: {
+                match: '='
+            },
+            link: function(scope, elem, attrs, ctrl) {
+                scope.$watch(function() {
+                    return (ctrl.$pristine && angular.isUndefined(ctrl.$modelValue)) || scope.match === ctrl.$modelValue;
+                }, function(currentValue) {
+                    ctrl.$setValidity('match', currentValue);
+                });
+            }
+        };
+    });
+
+
+
 // create angular app
 	var validationApp = angular.module('validationApp', []);
 
 	// create angular controller
 	validationApp.controller('mainController', function($scope) {
-
 		
-
 	});
+
+	
