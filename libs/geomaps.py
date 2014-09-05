567f2855b03632566cdd34066018a9581e788fd9
Ajan Lal Shrestha <ajan@nixify.com>
2014-09-05 22:50:53 +0545

    Fetched the static map and stored it to local for now. The location for the storing the map has to be updated to Amazon bucket.

--- libs/geomaps.py
+++ libs/geomaps.py
@@ -1,8 +1,11 @@
 
 
 
+import Image
 import logging as log
-import simplejson, urllib
+import simplejson
+import urllib
+from cStringIO import StringIO
 from django.conf import settings
 from pprint import pprint
 
@@ -48,6 +51,47 @@ class GMaps(object):
 		return __distance
 
 
+	def __obtain_center_location(self, bounds):
+		"""Obtain the center co-ordinates of the map
+		"""
+		northeast=bounds.get('northeast')
+		southwest=bounds.get('southwest')
+		lat = (max(northeast['lat'], southwest['lat']) + min(northeast['lat'], southwest['lat']))/2
+		lng = (max(northeast['lng'], southwest['lng']) + min(northeast['lng'], southwest['lng']))/2
+		center = "{0},{1}".format(lat, lng)
+		return center
+
+	def __obtain_static_image_url(self, center, start_location, end_location):
+		# zoom=10
+		size="640x640"
+		maptype="roadmap"
+		url="http://maps.googleapis.com/maps/api/staticmap?center={0}&size={1}&maptype={2}&markers=color:blue|{3}&markers=color:purple|{4}".format(center, size, maptype, start_location, end_location)
+		return url
+
+	def __save_image(self, bounds, start_location, end_location, filename):
+		"""Save the image
+		"""
+		url = self.__obtain_static_image_url(self.__obtain_center_location(bounds), start_location, end_location)
+		with open(filename, 'wb') as f:
+			buffer = StringIO(urllib.urlopen(url).read())
+			# f.write(urllib.urlopen(url).read())
+			image = Image.open(buffer)
+			image.save(f, "png")
+			# image.show()
+
+
+	def fetch_static_map(self, filename="content.png"):
+		if self.__status:
+			for route in self.__result['routes']:
+				bounds = route['bounds']
+				for leg in route['legs']:
+					start_location = leg['start_location']
+					start_location = "{0},{1}".format(start_location['lat'], start_location['lng'])
+					end_location = leg['end_location']
+					end_location = "{0},{1}".format(end_location['lat'], end_location['lng'])
+					self.__save_image(bounds, start_location, end_location, filename)
+
+
 if __name__ == "__main__":
 	gmaps = GMaps()
 	options = dict(
@@ -64,3 +108,4 @@ if __name__ == "__main__":
 	gmaps.set_geo_args(geo_args)
 	distance = gmaps.get_total_distance()
 	print distance
+	gmaps.get_maps() # fetch the static map
