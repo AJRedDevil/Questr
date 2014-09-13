660f97f312c27e4980e86b659e4dc380b4cbc7c7
Gaurav Ghimire <exsys@nixify.com>
2014-09-13 11:52:04 +0545

    Project directory cleanup
     - commented out django social auth context processors
     - removed bower_components
     - removed don’s templates as they’re not used now
     - updated requirements

--- questr/settings.py
+++ questr/settings.py
@@ -165,8 +165,8 @@ AUTHENTICATION_BACKENDS = (
 
 #Template context processors for social auth
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'social.apps.django_app.context_processors.backends',
-    'social.apps.django_app.context_processors.login_redirect',
+    # 'social.apps.django_app.context_processors.backends',
+    # 'social.apps.django_app.context_processors.login_redirect',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
 )
