c7ccc6c5d77cad380a95d4220edc10d2fc0b95dd
Gaurav Ghimire <exsys@nixify.com>
2015-03-12 04:28:12 +0545

    Pricing updates from Frank Integrated team page to landingpage

--- libs/pricing.py
+++ libs/pricing.py
@@ -22,9 +22,13 @@ class WebPricing(object):
     """
     def __init__(self):
         self.pricing_minutes = [10.0, 1.0,     1.0,      1.0,      0.8,      0.8,      0.8,      0.6,      0.6,      0.6]
+        self.pricing_Minutes_small    =   [6.0,   0.7,     0.7,      0.7,      0.7,      0.6,      0.6,      0.6,      0.6,      0.6]
+        self.pricing_Minutes_regular  =   [8.0,   0.8,     0.8,      0.8,      0.8,      0.7,      0.7,      0.7,      0.7,      0.7]
+        self.pricing_Minutes_large    =   [10.0,  1.0,     1.0,      1.0,      1.0,      0.8,      0.8,      0.8,      0.8,      0.8]
+
 
     def get_price(self, distance, size=0):
-        size = int(size)
+        SIZE = int(size)
         """
         Returns price depending on the type of shipment and distance.
         size 0 = filing bag
@@ -33,34 +37,71 @@ class WebPricing(object):
         """
         charge = 0.0
 
-        if distance <= 5.0:
-            charge = 10.0
-        else:
-            #Calculate shipping fee for the part that is divisible by 5
-            for i in xrange(0, int(math.ceil(distance/5))-1):
-                if i == 0:
-                    charge += self.pricing_minutes[0]
-                elif i >= 9:
-                    charge += self.pricing_minutes[9]*5
-                else:
-                    charge += self.pricing_minutes[i]*5
+        if SIZE is 0:
+            if distance <= 5.0:
+                charge = 6.0
+            else:
+                #Calculate shipping fee for the part that is divisible by 5
+                for i in xrange(0, int(math.ceil(distance/5))-1):
+                    if i == 0:
+                        charge += self.pricing_Minutes_small[0]
+                    elif i >= 9:
+                        charge += self.pricing_Minutes_small[9]*5
+                    else:
+                        charge += self.pricing_Minutes_small[i]*5
 
-        # Calculate shipping fee for the part that is not divisible by 5, aka the tail
-        if distance <= 50 and distance > 5:
-            charge += self.pricing_minutes[
-                int(math.ceil(distance / 5))-1] * float(distance % 5)
-        elif distance > 50:
-            charge += self.pricing_minutes[9] * float(distance % 5)
+            #Calculate shipping fee for the part that is not divisible by 5, aka the tail
+            if distance <= 50 and distance > 5:
+                charge += self.pricing_Minutes_small[int(math.ceil(distance/5))-1] * float(distance%5)
+            elif distance > 50:
+                charge += self.pricing_Minutes_small[9] * float(distance%5)
 
-        # #Round the shipping fee to 2 decimal places
-        charge = round(charge, 2)
+            #Round the shipping fee to 2 decimal places
+            charge = round(charge, 2)
 
-        #Sizes
-        if size == 0:  # filing bag
-            return round((charge*0.8), 2)
-        elif size == 1:  # moving box
-            return round(charge, 2)
-        elif size == 2:  # extra large
-            return round((charge*1.2), 2)
-        else:
-            return round((charge*0), 2)
+        elif SIZE is 1:
+            if distance <= 5.0:
+                charge = 8.0
+            else:
+                #Calculate shipping fee for the part that is divisible by 5
+                for i in xrange(0, int(math.ceil(distance/5))-1):
+                    if i == 0:
+                        charge += self.pricing_Minutes_regular[0]
+                    elif i >= 9:
+                        charge += self.pricing_Minutes_regular[9]*5
+                    else:
+                        charge += self.pricing_Minutes_regular[i]*5
+
+            #Calculate shipping fee for the part that is not divisible by 5, aka the tail
+            if distance <= 50 and distance > 5:
+                charge += self.pricing_Minutes_regular[int(math.ceil(distance/5))-1] * float(distance%5)
+            elif distance > 50:
+                charge += self.pricing_Minutes_regular[9] * float(distance%5)
+
+            #Round the shipping fee to 2 decimal places
+            charge = round(charge, 2)
+
+        elif SIZE is 2:
+            if distance <= 5.0:
+                charge = 12.0
+            else:
+                #Calculate shipping fee for the part that is divisible by 5
+                for i in xrange(0, int(math.ceil(distance/5))-1):
+                    if i == 0:
+                        charge += self.pricing_Minutes_large[0]
+                    elif i >= 9:
+                        charge += self.pricing_Minutes_large[9]*5
+                    else:
+                        charge += self.pricing_Minutes_large[i]*5
+
+            #Calculate shipping fee for the part that is not divisible by 5, aka the tail
+            if distance <= 50 and distance > 5:
+                charge += self.pricing_Minutes_large[int(math.ceil(distance/5))-1] * float(distance%5)
+            elif distance > 50:
+                charge += self.pricing_Minutes_large[9] * float(distance%5)
+
+            #Round the shipping fee to 2 decimal places
+            charge = round(charge, 2)
+
+
+        return charge
